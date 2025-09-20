4f158ce223e0772f9134bae470f4796af36d442b
pliniocflima <plinio.cesar.fl@gmail.com>
2025-09-20 16:26:24 -0300

    Update registrar-sw.js

--- docs/assets/registrar-sw.js
+++ docs/assets/registrar-sw.js
@@ -1,7 +1,7 @@
 // assets/registrar-sw.js
 if ('serviceWorker' in navigator) {
   // O Service Worker deve estar na raiz do escopo que ele controla.
-  const swPath = '/appEstoque/sw.js'
+  const swPath = '/appEstoque/assets/sw.js'
   const swScope = '/appEstoque/';
   window.addEventListener('load', () => {
     navigator.serviceWorker.register(swPath, { scope: swScope })
@@ -14,4 +14,5 @@ if ('serviceWorker' in navigator) {
   });
 } else {
   console.log('Service Worker não é suportado neste navegador.');
+
 }
