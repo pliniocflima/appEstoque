946ee492f6ba1dac346f7688092e33078aaba06a
pliniocflima <plinio.cesar.fl@gmail.com>
2025-12-30 17:17:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'estoque-casa-vBETA2';
+const CACHE_NAME = 'estoque-casa-vBETA3';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,6 +6,9 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+  // Força o SW a ativar imediatamente, sem esperar o usuário fechar a aba
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
@@ -14,29 +17,35 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    Promise.all([
+      // Faz o SW assumir o controle de todas as páginas abertas imediatamente
+      self.clients.claim(),
+      // Limpa caches antigos
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
+        // Cache-First Strategy
         if (response) {
           return response;
         }
         return fetch(event.request);
       })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
